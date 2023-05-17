72bdddd969a71651f465fe6a8e64e47494d2b1ae
Mohan Sardar <56884480+MohanSardar01@users.noreply.github.com>
2023-05-17 19:40:10 +0530

    Testbench

--- tb.sv
+++ tb.sv
@@ -1,13 +1,23 @@
 module tb();
-  
-  environment env;
-  
-  mailbox gdmbx, 
-  mailbox msmbx;
-  
-  initial begin
-    gdmbx = new();
-    msmbx = new();
-  end
-  
+ 
+environment env;
+ 
+mailbox gdmbx;
+mailbox msmbx;
+ 
+andt_intf vif();
+ 
+andt dut (vif.a, vif.b,vif.y);
+ 
+initial begin
+gdmbx = new();
+msmbx = new();
+env = new(gdmbx, msmbx);
+env.vif = vif;
+env.run();
+#200;
+$finish;
+end
+ 
 endmodule
+ 
