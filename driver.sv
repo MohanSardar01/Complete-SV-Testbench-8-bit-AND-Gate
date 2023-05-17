a6fffcdc7ddc855d418cd8cb171b15cd6848f531
Mohan Sardar <56884480+MohanSardar01@users.noreply.github.com>
2023-05-17 17:58:21 +0530

    driver class

--- driver.sv
+++ driver.sv
@@ -1,1 +1,24 @@
-
+class driver;
+  transaction t;
+  mailbox mbx;
+  event done;
+  
+  virtual andt_intf vif;
+  
+  function new(mailbox mbx);
+    this.mbx = mbx;
+  endfuncton
+    
+    task run();
+      t = new();
+      forever begin
+        mbx.get(t);
+        vif.a = t.a;
+        vif.b = t.b;
+        $display("[DRV]: data sent to interface");
+        -> done;
+        #10;
+      end
+    endtask
+    
+endclass
